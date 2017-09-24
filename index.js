e39e5bea00c2286a23b61bf1de0c082915261acc
James Royer <walkingalchemy@gmail.com>
2017-09-24 18:15:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+  Object.assign({},object,{[key]:value})
+  //object[key] = value
+  //return object
 }
